22e21a2bb7de4d40f2d5cb92c0bfbf6865457de1
Sambhaji Patil <sampat20th@gmail.com>
2024-11-28 23:58:57 +0530

    update

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const port = 3000;
 
 require("dotenv").config();
 
-// Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Serve static files
 app.use(express.static('public'));
 app.use('/styles', express.static(path.join(__dirname, 'public')));
 
@@ -24,7 +22,7 @@ app.get('/api/weather', async (req, res) => {
     const { lat, lon } = req.query;
     console.log(lat);
     console.log(lon);
-    const apiKey = process.env.openweathermap_api_key;// Replace with your actual API key
+    const apiKey = process.env.openweathermap_api_key;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
     try {
@@ -36,19 +34,16 @@ app.get('/api/weather', async (req, res) => {
     }
 });
 
-// Tourism API endpoint (example using a mock API)
-// Tourism API endpoint
-// Tourism API endpoint 
 app.get('/api/tourism', async (req, res) => {
     const { lat, lon } = req.query;
-    const apiKey2 = process.env.tourism_api_key; // Add your actual API key here
+    const apiKey2 = process.env.tourism_api_key;
     const url2 = `https://api.geoapify.com/v2/place-details?lat=${lat}&lon=${lon}&features=radius_500,radius_500.atm,radius_500.restaurant,drive_5,drive_5.fuel&apiKey=${apiKey2}`;
  
     try {
         const response = await axios.get(url2);
         const features = response.data.features;
 
-        // Filter restaurants, ATMs, and fuel stations
+        // Filter restaurants, ATMs, and fuel stations in 500 meters radius
         const restaurants = features.filter(feature => feature.properties.feature_type === "radius_500.restaurant");
         const atms = features.filter(feature => feature.properties.feature_type === "radius_500.atm");
         const fuelStations = features.filter(feature => feature.properties.feature_type === "drive_5.fuel");
